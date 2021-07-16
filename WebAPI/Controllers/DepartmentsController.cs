f4b8c0f66b082a5a27497edb26f25f18d8d42614
adriandamiaosoitic <86964732+adriandamiaosoitic@users.noreply.github.com>
2021-07-16 10:26:09 -0300

    First Model-View-Controller Department

---
+++ WebAPI/Controllers/DepartmentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Department> list = new List<Department>();
+
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Computers" });
+            list.Add(new Department { Id = 3, Name = "Smart Home" });
+            list.Add(new Department { Id = 4, Name = "Home and Kitchen" });
+            list.Add(new Department { Id = 5, Name = "Tools" });
+            list.Add(new Department { Id = 6, Name = "Video Games" });
+
+            return View(list);
+        }
+    }
+}
