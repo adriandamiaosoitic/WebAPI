c68f43916d73416ad13a2e66aba699270ebdc01b
adriandamiaosoitic <86964732+adriandamiaosoitic@users.noreply.github.com>
2021-07-16 12:43:26 -0300

    Sellers Controller

---
+++ WebAPI/Services/SellerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public class SellerService
+    {
+        private WebAPIContext _context;
+            
+        public SellerService(WebAPIContext context) //Injeção de dependência
+        {
+            _context = context;
+        }
+    }
+}
