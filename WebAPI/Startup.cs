bfcaa429b7ab4e41390ac2a5b00987c49b43b263
adriandamiaosoitic <86964732+adriandamiaosoitic@users.noreply.github.com>
2021-07-16 10:44:04 -0300

    MySQL adaptation and first migration

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -37,7 +37,8 @@ namespace WebAPI
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebAPIContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebAPIContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebAPIContext"), builder =>
+                    builder.MigrationsAssembly("WebAPI")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
