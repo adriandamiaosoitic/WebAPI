9e3d1f3fa1e7e473820bd11728781163a75b2e8d
adriandamiaosoitic <86964732+adriandamiaosoitic@users.noreply.github.com>
2021-07-20 09:26:44 -0300

    Preparing Sales Search Navigation Views

---
+++ WebAPI/Controllers/SalesRecordsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly WebAPIContext _context;
+
+        public SalesRecordsController(WebAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SalesRecords
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.SalesRecord.ToListAsync());
+        }
+
+        public async Task<IActionResult> SimpleSearch()
+        {
+            return View();
+        }
+        public async Task<IActionResult> GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
